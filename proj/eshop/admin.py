dda32c62dc692434156b6186eb135a89771cd7fe
Alexei <lexx.kg@gmail.com>
2019-11-30 15:08:20 +0600

    show custom CartContent M2M relation in admin

--- proj/eshop/admin.py
+++ proj/eshop/admin.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
 from . import models as m
 
+class CartContentInline(admin.TabularInline):
+    model = m.CartContent
+    extra = 1
+
+class CartAdmin(admin.ModelAdmin):
+    inlines = (CartContentInline,)
+
 admin.site.register(m.Product)
 admin.site.register(m.Category)
-admin.site.register(m.Cart)
+admin.site.register(m.Cart, CartAdmin)
+admin.site.register(m.CartContent)
 admin.site.register(m.Order)
 
 # Register your models here.
