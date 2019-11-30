60e3ceff53fd705513f7ee767ba6f95fc27a628b
Alexei <lexx.kg@gmail.com>
2019-11-30 19:01:38 +0600

    output cat slector and cat products in category view

--- proj/eshop/urls.py
+++ proj/eshop/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('category', views.cats, name='products_category_all'),
     path('category/<slug:slug>', views.cats, name='products_category'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- proj/eshop/views.py
+++ proj/eshop/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Product
+from .models import Product, Category
 
 # Create your views here.
 def index(request):
     return render(request, 'main.html')
 
-def cats(request, slug):
-    p = Product.objects.all()
-    print(">>>>>>", slug)
-    context = { 'p': p }
+def cats(request, slug=None):
+    cats = Category.objects.all()
+    if slug:
+        selected_cat = Category.objects.get(slug=slug)
+        products = Product.objects.filter(category_id=selected_cat.id).all()
+    else:
+        products = []
+
+    print(">>>>>>", slug, cats)
+    context = { 'cats': cats, 'products': products }
     return render(request, 'cats.html', context)
